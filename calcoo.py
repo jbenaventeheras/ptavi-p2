4db33054fe73e14f921530d0cfb3687295de3e97
Javier Benavente Heras <javierbh@alpha01.aulas.gsyc.urjc.es>
2018-09-26 18:34:52 +0200

    versio1clacoo

--- calcoo.py
+++ calcoo.py
@@ -1,12 +1,28 @@
-"DP"
-
 import sys
 
+class Calculadora(object):
 
-class Calculadora():
-    def suma(self, op1, op2):
-        return op1 + op2
 
-    def resta(self, op1, op2):
-        return op1 - op2
+    def suma(self, operando1, operando2):
+        return operando1 + operando2
 
+    def resta(self, operando1, operando2):
+        return operando1 - operando2
+
+if __name__ == "__main__":
+
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    Usarcalc = Calculadora()
+
+    if sys.argv[2] == "suma":
+        resultado = calc.suma(operando1, operando2)
+    elif operacion == "resta":
+       sys.argv[2] = calc.resta(operando1, operando2)
+    else:
+        sys.exit("operacion no valida")
+    print(resultado)
